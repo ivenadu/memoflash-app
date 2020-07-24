db58e8489e89bce4595c9814338cb2e2291b3a14
ivena <ivena2012@gmail.com>
2020-07-24 14:39:09 -0700

    Created methods and tests for Flashcard class

--- src/main/model/Flashcard.java
+++ src/main/model/Flashcard.java
@@ -1,4 +1,34 @@
 package model;
 
 public class Flashcard {
+    private String question;
+    private String answer;
+
+    public Flashcard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    // EFFECTS: returns the question and answer of the flashcard
+    public String getQA() {
+        return "Question: " + getQuestion() + "\nAnswer: " + getAnswer();
+    }
+
+    // EFFECTS: returns the question
+    public String getQuestion() {
+        return this.question;
+    }
+
+    // EFFECTS: returns the answer
+    public String getAnswer() {
+        return answer;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: edits the card's question and answer
+    public void editCard(String editQ, String editA) {
+        this.question = editQ;
+        this.answer = editA;
+
+    }
 }
--- src/test/model/FlashcardTest.java
+++ src/test/model/FlashcardTest.java
@@ -1,7 +1,29 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FlashcardTest {
-    // delete or rename this class!
+
+    private Flashcard card;
+
+    @BeforeEach
+    public void preTest(){
+        card = new Flashcard("Question", "Answer");
+    }
+
+    @Test
+    public void getQATest(){
+        assertEquals(card.getQA(), "Question: Question\nAnswer: Answer");
+    }
+
+    @Test
+    public void editCardTest(){
+        card.editCard("editedQ", "editedA");
+        assertEquals(card.getQuestion(),"editedQ");
+        assertEquals(card.getAnswer(), "editedA");
+    }
+
 }
