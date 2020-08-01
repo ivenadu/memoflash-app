da85a68d619d8102b1146615a01497a14e121a72
ivena <ivena2012@gmail.com>
2020-08-01 02:04:56 -0700

    added jackson to lib

---
+++ src/main/persistence/Write.java
@@ -0,0 +1,16 @@
+package persistence;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.DeckCollection;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * Represents a class that is able to write instances to JSON file
+ */
+public class Write {
+
+
+}
