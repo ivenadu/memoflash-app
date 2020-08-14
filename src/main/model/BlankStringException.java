bb5703e401c3a670457ca5bb29429cd0535e78bb
ivenadu <64575243+ivenadu@users.noreply.github.com>
2020-08-13 18:00:50 -0700

    complete

--- src/main/model/BlankStringException.java
+++ src/main/model/BlankStringException.java
@@ -1,5 +1,8 @@
 package model;
 
+/**
+ * An exception for blank string
+ */
 public class BlankStringException extends Exception {
     public BlankStringException() {
     }
--- src/main/model/NonDistinctException.java
+++ src/main/model/NonDistinctException.java
@@ -1,5 +1,8 @@
 package model;
 
+/**
+ * An exception for distinct inputs
+ */
 public class NonDistinctException extends Exception {
     public NonDistinctException() {
     }
