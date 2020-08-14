524b54151a7c6f2e0f80dbfe7db4cdd61a4a2a72
ivenadu <64575243+ivenadu@users.noreply.github.com>
2020-08-13 17:33:55 -0700

    edit diagram

--- src/main/ui/gui/GuiMain.java
+++ src/main/ui/gui/GuiMain.java
@@ -1,10 +1,5 @@
 package ui.gui;
 
-import javax.swing.*;
-import java.io.IOException;
-
-import persistence.Load;
-
 import static javax.swing.SwingUtilities.invokeLater;
 
 /**
@@ -14,17 +9,7 @@ import static javax.swing.SwingUtilities.invokeLater;
 //SOURCES REFERRED TO: https://docs.oracle.com/javase/tutorial/uiswing/components/list.html (ListDemo.java)
 
 public class GuiMain {
-    private static String deckName;
 
-    static {
-        try {
-            deckName = Load.loadFile("./data/myFile.json").getActiveDeck().getTitle();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // MODIFIES: this
     // EFFECTS: run program of GUI
     public static void main(String[] args) {
         invokeLater(() -> new GUImaker());
